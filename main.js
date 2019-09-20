e319310cd4affb868a0876e15f944b3378a73177
nath54 <cerisara.nathan@gmail.com>
2019-09-20 17:40:26 +0100

    a

--- main.js
+++ main.js
@@ -7,35 +7,65 @@ cam=[-50,-50];
 
 f=document.getElementById("fonction").value;
 p=document.getElementById("precision").value;
-min=document.getElementById("min").value;
-max=document.getElementById("max").value;
+vmin=document.getElementById("min").value;
+vmax=document.getElementById("max").value;
 color=document.getElementById("color").value;
 
-min=parseInt(min);
-max=parseInt(max);
+vmin=parseInt(vmin);
+vmax=parseInt(vmax);
 p=parseFloat(p);
 
 function ff(x){
 	return x*2;
 }
 
-function calc(){
-	valeurs=[];
-	aa=(max-min)/p;
-	for( x=0 ; x<aa ; x++){
-		z=min+x;
-		valeurs.push( ff(z) );
-    }
-    return valeurs;
-}
-
-valeurs=calc();
+valeurs=[];
 
 function aff(){
 	context.fillStyle="rgb(250,250,250)";
 	context.fillRect(0,0,tex,tey);
 	context.beginPath();
-	
+	context.moveTo(cam[0]+0,cam[1]+vmin);
+	context.lineTo(cam[0]+0,cam[1]+vmax);
+	context.stroke();
+	context.closePath();
+	context.beginPath();
+	context.moveTo(cam[0]+vmin,cam[1]+0);
+	context.lineTo(cam[0]+vmax,cam[1]+0);
+	context.stroke();
+	context.closePath();
+	context.strokeStyle=color;
+	context.beginPath();
+	context.moveTo( cam[0]+vmin , cam[1]+valeurs[0] );
+	xx=vmin+p;
+	for( v in valeurs ){
+	    context.lineTo( cam[0]+xx , cam[1]+v );
+	    xx+=p;
+	}
+	context.stroke();
+	context.closePath();
+}
+
+function calc(){
+	valeurs=[];
+	aa=(vmax-vmin)/p;
+	for( x=0 ; x<aa ; x++){
+		z=vmin+x;
+		valeurs.push( ff(z) );
+    }
+    aff();
+    return valeurs;
+}
+
+valeurs=calc();
+
+function move(direction){
+    if( direction == "Up") cam[1]-=vit;
+    else if( direction == "Down") cam[1]+=vit;
+    else if( direction == "Left") cam[0]-=vit;
+    else if( direction == "Right") cam[0]+=vit;
+    aff();
 }
 
 aff();
+
